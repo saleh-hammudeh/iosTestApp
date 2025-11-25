dbd8981c7c61b2dc501193940ead5341edb3ed41
saleh hammudeh <hammudehsaleh@gmail.com>
2025-11-25 19:22:23 +0200

    Hello, ios course

---
+++ testapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by MBpro on 11/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, ios course!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by MBpro on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
